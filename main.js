c38809fd947a232b7622e5813a8094fe812ae312
wcout <wcout@users.noreply.github.com>
2018-04-21 19:21:45 +0200

    +messing around with missile fireing - TBC

--- main.js
+++ main.js
@@ -22,6 +22,7 @@ var ground_grad;
 var sky_grad;
 var bg_grad;
 var paused = false;
+var repeated_right = -1;
 
 
 class Fl_Rect
@@ -32,6 +33,63 @@ class Fl_Rect
 		this.h = height;
 	}
 }
+
+function fl_font( family, size )
+{
+	var s = family.split( ' ' );
+	var f = '';
+	if ( s.length >= 2 )
+	{
+		f = s[1] + ' ';
+	}
+	f += size + 'px ' +  s[0];
+	ctx.font = f;
+}
+
+function fl_draw( text, x, y )
+{
+	ctx.fillText( text, x, y );
+}
+
+function fl_color( c )
+{
+	ctx.strokeStyle = c;
+	ctx.fillStyle = c;
+}
+
+function fl_xyline( x0, y, x1 )
+{
+	ctx.beginPath();
+	ctx.moveTo( x0, y );
+	ctx.lineTo( x1, y );
+	ctx.stroke();
+}
+
+function fl_yxline( x, y0, y1 )
+{
+	ctx.beginPath();
+	ctx.moveTo( x, y0 );
+	ctx.lineTo( x, y1 );
+	ctx.stroke();
+}
+
+function fl_line_style( type, width )
+{
+	if ( width )
+	{
+		ctx.lineWidth = width;
+	}
+	else
+	{
+		ctx.lineWidth = 1;
+	}
+}
+
+function fl_rectf( x, y, w, h )
+{
+	ctx.fillRect( x, y, w, h );
+}
+
 
 class ObjInfo
 {
@@ -44,12 +102,31 @@ class ObjInfo
 		this.frames = frames;
 		this.curr_frame = 0;
 		this.cnt = 0;
-		this.image_width = this.image.width / this.frames;
+		if ( this.image )
+		{
+			this.image_width = this.image.width / this.frames;
+		}
+		if ( this.type == 256 )
+		{
+			this.image_width = 60;
+			console.log( "missile created %d/%d", this.x, this.y );
+		}
 	}
 
 	draw()
 	{
 		var x = this.x - ox; // x-coord. on screen
+
+		if ( this.type == 256 )
+		{
+			console.log( "draw missile" );
+			fl_color( '#ffffff' );
+			fl_line_style( 1, 3 );
+			fl_xyline( x, this.y, x + 60, this.y );
+			fl_line_style( 0, 0 );
+			return;
+		}
+
 		if ( this.frames == 1 )
 		{
 			ctx.drawImage( this.image, x, this.y );
@@ -143,10 +220,16 @@ function create_landscape()
 
 }
 
-function onKeyDown()
+function fireMissile()
 {
-	var k = keysDown;
-	if ( k[57] ) // '9'
+	var obj = new ObjInfo( 256, ox + shipX + ship.width + 20, shipY + ship.height/2+7, null );
+	objects.push( obj );
+	missile_sound.play();
+}
+
+function onKeyDown( k )
+{
+	if ( k == 57 ) // '9'
 	{
 		paused = !paused;
 		if ( !paused )
@@ -159,78 +242,47 @@ function onKeyDown()
 			bg_music.stop();
 		}
 	}
+	if ( k == 39 || k == 80 )
+	{
+		console.log( "**reset repeated_right**" );
+		repeated_right = -5;
+	}
+}
+
+function onKeyUp( k )
+{
+	if ( k == 32 )
+	{
+	}
+	if ( k == 39 || k == 80 )
+	{
+		if ( repeated_right <= 0 )
+		{
+			console.log( "repeated_right: %d\n", repeated_right );
+			fireMissile();
+		}
+	}
 }
 
 function onEvent( e )
 {
 	if ( e.type == "keydown" )
 	{
+		if ( !keysDown[e.keyCode] )
+		{
+			onKeyDown( e.keyCode );
+		}
 		keysDown[e.keyCode] = true;
-		onKeyDown();
 		e.preventDefault();
 	}
 	if ( e.type == "keyup" )
 	{
 		keysDown[e.keyCode] = false;
+		onKeyUp( e.keyCode );
 		e.preventDefault();
 	}
 }
 
-function fl_font( family, size )
-{
-	var s = family.split( ' ' );
-	var f = '';
-	if ( s.length >= 2 )
-	{
-		f = s[1] + ' ';
-	}
-	f += size + 'px ' +  s[0];
-	ctx.font = f;
-}
-
-function fl_draw( text, x, y )
-{
-	ctx.fillText( text, x, y );
-}
-
-function fl_color( c )
-{
-	ctx.strokeStyle = c;
-	ctx.fillStyle = c;
-}
-
-function fl_xyline( x0, y, x1 )
-{
-	ctx.beginPath();
-	ctx.moveTo( x0, y );
-	ctx.lineTo( x1, y );
-	ctx.stroke();
-}
-
-function fl_yxline( x, y0, y1 )
-{
-	ctx.beginPath();
-	ctx.moveTo( x, y0 );
-	ctx.lineTo( x, y1 );
-	ctx.stroke();
-}
-
-function fl_line_style( type, width )
-{
-	if ( width )
-	{
-		ctx.lineWidth = width;
-	}
-	else
-	{
-		ctx.lineWidth = 1;
-	}
-}
-
-function fl_rectf( x, y, w, h )
-{
-	ctx.fillRect( x, y, w, h );
-}
 
 function drawObjects()
 {
@@ -262,6 +314,10 @@ function drawObjects()
 			else if ( o.type == 16 )
 			{
 				o.update();
+			}
+			else if ( o.type == 256 )
+			{
+				o.x += 4 * dx;
 			}
 		}
 	}
@@ -327,7 +383,12 @@ function update()
 	var k = keysDown;
 	if ( k[39] || k[80] )
 	{
-		shipX += dx;
+		repeated_right++;
+		console.log( "++repeated_right: %d\n", repeated_right );
+		if ( repeated_right > 0 )
+		{
+			shipX += dx;
+		}
 	}
 	if ( k[37] || k[79])
 	{
@@ -350,7 +411,6 @@ function update()
 		fl_line_style( 0, 0 );
 		missile_sound.play();
 	}
-
 	ox += dx;
 	if ( ox + Screen.clientWidth >= LS.length )
 	{
